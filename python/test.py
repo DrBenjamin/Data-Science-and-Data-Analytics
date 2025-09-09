1e93cfe1dd928b4a47c53e0040506ad4619706aa
DrBenjamin <ben@seriousbenentertainment.org>
2025-09-09 11:16:05 +0200

    added python script

---
+++ python/test.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+# Creating a simple DataFrame
+pdf = pd.DataFrame({
+    "a": [1, 2, 3],
+    "b": [4.5, 4.5, 4.5],
+    "c": ["five", "five", "five"]
+})
+
+# Printing the DataFrame
+print(pdf)
+
+# Removing the pdf variable
+#del pdf
