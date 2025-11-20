ff460b2714af79838b4c830ff44c313763e3a09f
DrBenjamin <ben@seriousbenentertainment.org>
2025-11-20 08:39:33 +0100

    adjusted time

--- scripts/adjustTime.lua
+++ scripts/adjustTime.lua
@@ -14,8 +14,8 @@ function Meta(meta)
   end
 
   if should_set then
-    -- compute current time + 2 hours (seconds)
-    local t = os.time() + 2 * 60 * 60
+    -- compute current time + 1 hours (seconds)
+    local t = os.time() + 1 * 60 * 60
     -- format as UTC time then append +02:00 offset so Quarto parses as local+02
     local iso = os.date("!%d.%m.%Y %H:%M", t)
     meta.date = pandoc.MetaString(iso)
